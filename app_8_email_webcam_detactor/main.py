4db3ab042cebeaba8b69ab90b0d9f6c7a7b4441d
Faraz <farazmoin.uk@gmail.com>
2025-12-27 17:06:22 +0530

    now detects when a new object enters the view.

--- app_8_email_webcam_detactor/main.py
+++ app_8_email_webcam_detactor/main.py
@@ -3,9 +3,29 @@ import time
 
 time.sleep(1)
 video = cv2.VideoCapture(0)
+first_frame = None
 while True:
     check, frame = video.read()
-    cv2.imshow('My video', frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    counters, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for counter in counters:
+        if cv2.contourArea(counter) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(counter)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+
+    cv2.imshow('My second frame', frame)
 
     key = cv2.waitKey(1)
 
